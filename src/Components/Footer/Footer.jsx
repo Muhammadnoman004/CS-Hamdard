d623098b0e48a6e80a8ae7c23b96e39634ce54cd
Noman Shamim <nomanshamim2004@gmail.com>
2024-10-04 00:47:34 +0500

    still working on footer....

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,8 +5,30 @@ export default function Footer() {
     return (
         <section>
             <div>
-                <div className='bg-black w-full h-96'>
+                <div className='absolute w-full'>
                     <img src={hamdard} className='w-full h-96 object-cover opacity-20' alt="#" />
+                </div>
+                <div className='bg-black w-full h-96 flex justify-between p-5'>
+                    <div>
+                        <h5 className='text-white'>Hamdard University</h5>
+                    </div>
+                    <div>
+                        <h5 className='text-white'>Admissions</h5>
+                        <div className='bg-yellow-500 h-56 w-1.5 relative bottom-8 right-5'></div>
+                    </div>
+                    <div>
+                        <h5 className='text-white'>USEFUL LINKS</h5>
+                        <div className='bg-yellow-500 h-56 w-1.5 relative bottom-8 right-5'></div>
+                    </div>
+                    <div>
+                        <h5 className='text-white'>Faculties</h5>
+                        <div className='bg-yellow-500 h-56 w-1.5 relative bottom-8 right-5'></div>
+                    </div>
+                    <div>
+                        <h5 className='text-white'>Find Us</h5>
+                        <div className='bg-yellow-500 h-56 w-1.5 relative bottom-8 right-5'></div>
+
+                    </div>
                 </div>
                 <div className='bg-yellow-500 w-full h-28'>
 
