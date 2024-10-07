bd38d29286624543a000e0d3ac251db690a7d376
Noman Shamim <nomanshamim2004@gmail.com>
2024-10-08 00:40:54 +0500

    still working...

--- src/Pages/SoftwareEngineer.jsx
+++ src/Pages/SoftwareEngineer.jsx
@@ -12,19 +12,19 @@ const items = [
     },
     {
         key: '2',
-        label: "Structure of BSCS Programme"
+        label: "BS Mapping of PEOs to PLOs"
     },
     {
         key: '3',
-        label: "BSCS Road Map"
+        label: "Structure of BS SE Programme"
     },
     {
         key: '4',
-        label: "BSCS (Biology Group) Road Map"
+        label: "BS SE Roadmap"
     },
     {
         key: '5',
-        label: "BSCS Fee Structure"
+        label: "BS SE Fee Structure"
     },
 ];
 
@@ -36,15 +36,54 @@ const items2 = [
     },
     {
         key: '2',
-        label: "Structure of MSCS Programme"
+        label: "Structure of MS Data Science"
     },
     {
         key: '3',
-        label: "MSCS Road Map"
+        label: "MS Data Science Road Map"
     },
     {
         key: '4',
-        label: "MSCS Fee Structure"
+        label: "MS Data Science Fee Structure"
+    },
+];
+
+const items3 = [
+    {
+        key: '1',
+        label: "Dean's Message"
+    },
+    {
+        key: '2',
+        label: "HOD's Message"
+    },
+    {
+        key: '3',
+        label: "Program Offered"
+    },
+    {
+        key: '4',
+        label: "Program Educational Objectives (PEO's)"
+    },
+    {
+        key: '5',
+        label: "Program Learning Objectives (PLO's)"
+    },
+    {
+        key: '6',
+        label: "Faculty"
+    },
+    {
+        key: '7',
+        label: "News and Events"
+    },
+    {
+        key: '8',
+        label: "ESSE Societies"
+    },
+    {
+        key: '9',
+        label: "Publications"
     },
 ];
 
@@ -81,42 +120,32 @@ export default function SoftwareEngineer() {
                             </Dropdown>
                         </div>
                         <div>
+                            <button className='p-2 w-56 bg-green-900 text-white text-start mb-2 px-3 hover:bg-yellow-500 font-semibold focus:bg-yellow-500'>ADP Software Engineering Roadmap</button>
+                        </div>
+                        <div>
                             <Dropdown
                                 menu={{
-                                    items: items2,
+                                    items: items2
                                 }}
                                 placement="bottomLeft"
                                 arrow={{
                                     pointAtCenter: true,
                                 }}
                             >
-                                <button className='p-2 w-56 bg-green-900 text-white text-start mb-2 px-3 hover:bg-yellow-500 font-semibold focus:bg-yellow-500'>MS Computer Science Weekend 2 Years <FaAngleRight /></button>
+                                <button className='p-2 w-56 bg-green-900 text-white text-start mb-2 px-3 hover:bg-yellow-500 font-semibold focus:bg-yellow-500'>MS Data Science <FaAngleRight /></button>
                             </Dropdown>
                         </div>
                         <div>
                             <Dropdown
                                 menu={{
-                                    items
+                                    items: items3
                                 }}
                                 placement="bottomLeft"
                                 arrow={{
                                     pointAtCenter: true,
                                 }}
                             >
-                                <button className='p-2 w-56 bg-green-900 text-white text-start mb-2 px-3 hover:bg-yellow-500 font-semibold focus:bg-yellow-500'>MS Artificial Intelligence <FaAngleRight /></button>
-                            </Dropdown>
-                        </div>
-                        <div>
-                            <Dropdown
-                                menu={{
-                                    items
-                                }}
-                                placement="bottomLeft"
-                                arrow={{
-                                    pointAtCenter: true,
-                                }}
-                            >
-                                <button className='p-2 w-56 bg-green-900 text-white text-start mb-2 px-3 hover:bg-yellow-500 font-semibold focus:bg-yellow-500'>About Department Of Computer Science <FaAngleRight /></button>
+                                <button className='p-2 w-56 bg-green-900 text-white text-start mb-2 px-3 hover:bg-yellow-500 font-semibold focus:bg-yellow-500'>About <FaAngleRight /></button>
                             </Dropdown>
                         </div>
                     </div>
@@ -131,13 +160,6 @@ export default function SoftwareEngineer() {
                         <p className='pt-3 pb-2 text-lg'>To create and impart the latest Computer Science and interdisciplinary knowledge in a way that inspires its students to be ethical lifelong learners, researchers and innovative problem solvers who strive to improve others' lives both locally and globally.</p>
                         <h3 className='font-bold text-3xl'>Core Values</h3>
                         <p className='pt-3 pb-2 text-lg'>The core values guiding the department are:</p>
-                        <ul className='list-disc text-lg'>
-                            <li>Commitment to high-quality undergraduate education and student success.</li>
-                            <li>Quality assurance of academic programs through disciplinary accreditation.</li>
-                            <li>Continuous program improvement through assessment of program educational objectives and student outcomes.</li>
-                            <li>New initiatives and programs with changes in technology and global interdependence.</li>
-                            <li>Continued professional development of faculty members and scholarly contributions to the discipline.</li>
-                        </ul>
                     </div>
                 </div>
 
