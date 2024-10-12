82e394f0343e0883ff3689ae6d93f162b1175cc5
Noman Shamim <nomanshamim2004@gmail.com>
2024-10-12 16:28:21 +0500

    Some Navbar issue solved.

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
 
   return (
     <>
-      {/* <ComputerScience /> */}
-      <SoftwareEngineer />
+      <ComputerScience />
+      {/* <SoftwareEngineer /> */}
     </>
   )
 }
--- src/Components/Navbars/Navbars.jsx
+++ src/Components/Navbars/Navbars.jsx
@@ -17,21 +17,47 @@ export default function Navbars() {
                         <Nav.Link className='hover:underline underline-offset-8 decoration-2' href="#about">About</Nav.Link>
                         <NavDropdown className='hover:underline underline-offset-8 decoration-2' title="Academics" id="collapsible-nav-dropdown">
                             <NavDropdown.Item className='capitalize' href="#action/3.1">Faculty of Social Sciences</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item className='capitalize' href="#action/3.2">Faculty of Computer Sciences</NavDropdown.Item>
-                            <NavDropdown.Divider />
+                            <NavDropdown
+                                title="Faculty of Computer Sciences"
+                                id="nested-dropdown"
+                                className='capitalize'
+
+                            >
+                                <NavDropdown.Item href="#action/3.2.1">Department of Computer Science</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2.2">Department of Information Technology</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2.2">Department of Software Engineering</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                            </NavDropdown>
                             <NavDropdown.Item className='capitalize' href="#action/3.3">Faculty of Languages</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item className='capitalize' href="#action/3.4">Faculty of Basic Sciences</NavDropdown.Item>
                         </NavDropdown>
                         <NavDropdown className='hover:underline underline-offset-8 decoration-2' title="Offices" id="collapsible-nav-dropdown">
-                            <NavDropdown.Item className='capitalize' href="#action/3.1">Faculty of Social Sciences</NavDropdown.Item>
+                            <NavDropdown.Item className='capitalize' href="#action/3.1">Register Office</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item className='capitalize' href="#action/3.2">Faculty of Computer Sciences</NavDropdown.Item>
+                            <NavDropdown.Item className='capitalize' href="#action/3.2">Academics Branch</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item className='capitalize' href="#action/3.3">Faculty of Languages</NavDropdown.Item>
+                            <NavDropdown.Item className='capitalize' href="#action/3.3">Office of Internal Auditor (DIA)</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item className='capitalize' href="#action/3.4">Faculty of Basic Sciences</NavDropdown.Item>
+                            <NavDropdown.Item className='capitalize' href="#action/3.4">Treasurer</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item className='capitalize' href="#action/3.5">Human Resource Office (HR)</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item className='capitalize' href="#action/3.6">Information Technology (IT)</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item className='capitalize' href="#action/3.7">Quality Enhancement Cell (QEC)</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item className='capitalize' href="#action/3.8">Enterprise Resource Planning (ERP)</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item className='capitalize' href="#action/3.9">Student Affairs & Counselling (SA&C)</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item className='capitalize' href="#action/3.10">Office of Research, Innovation and Commercialization (ORIC)</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item className='capitalize' href="#action/3.11">Procurement & Purchase Department (P&LP)</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item className='capitalize' href="#action/3.12">Project Department</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item className='capitalize' href="#action/3.13">IBTIDA-Bussiness Incubation Center</NavDropdown.Item>
                         </NavDropdown>
                         <Nav.Link className='hover:underline underline-offset-8 decoration-2' href="#admissions">Admissions</Nav.Link>
                         <Nav.Link className='hover:underline underline-offset-8 decoration-2' href="#apply Now">Apply Now</Nav.Link>
